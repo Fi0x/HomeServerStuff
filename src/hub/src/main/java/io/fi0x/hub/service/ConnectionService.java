0528dd5bfcbe82f7270a18a7fd9a93b35a867628
Felix Pechtl <felix.pechtl@ametras.com>
2024-11-11 15:40:22 +0100

    Updated service registering logic

--- src/hub/src/main/java/io/fi0x/hub/service/ConnectionService.java
+++ src/hub/src/main/java/io/fi0x/hub/service/ConnectionService.java
@@ -1,5 +1,7 @@
 package io.fi0x.hub.service;
 
+import io.fi0x.javadatastructures.Tuple;
+import io.fi0x.javadatastructures.TupleList;
 import io.fi0x.util.dto.ServiceDataDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -16,7 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ConnectionService
 {
-	private static final List<ServiceDataDto> SERVICES = new ArrayList<>();
+	private static final TupleList<Long, ServiceDataDto> SERVICES = new TupleList<>();
 
 	public void registerService(String address, ServiceDataDto service)
 	{
@@ -40,13 +41,46 @@ public class ConnectionService
 			throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Unknown service-port");
 		}
 
-		SERVICES.add(service);
+		List<Tuple<Long, ServiceDataDto>> alreadySaved = SERVICES.stream()
+																 .filter(serviceDataDto -> serviceDataDto.getObject2()
+																										 .getName()
+																										 .equals(service.getName()))
+																 .toList();
+
+		if (alreadySaved.isEmpty())
+			SERVICES.add(new Tuple<>(System.currentTimeMillis(), service));
+
+		if (alreadySaved.size() > 1)
+		{
+			log.warn("Multiple services with the name '{}' were registered. The outdated ones will get removed",
+					 service.getName());
+			SERVICES.removeAll(alreadySaved);
+
+			SERVICES.add(new Tuple<>(System.currentTimeMillis(), service));
+			return;
+		}
+
+		Tuple<Long, ServiceDataDto> tuple = alreadySaved.get(0);
+		boolean sameIp = tuple.getObject2().getIp().equals(service.getIp());
+		boolean samePort = tuple.getObject2().getPort().equals(service.getPort());
+		if (sameIp && samePort)
+		{
+			tuple.setObject1(System.currentTimeMillis());
+			log.debug("Service '{}' updated", service.getName());
+		} else
+		{
+			log.info("Service '{}' changed from {}:{} to {}:{}", service.getName(), tuple.getObject2().getIp(),
+					 tuple.getObject2().getPort(), service.getIp(), service.getPort());
+			tuple.setObject2(service);
+		}
 	}
 
 	public List<ServiceDataDto> getConnectedServices()
 	{
 		log.trace("getConnectedServices() called");
 
-		return SERVICES;
+		return SERVICES.stream().map(Tuple::getObject2).toList();
 	}
+
+	//TODO: Add a scheduled task that clears the services-list from outdated services that didn't update recently
 }
