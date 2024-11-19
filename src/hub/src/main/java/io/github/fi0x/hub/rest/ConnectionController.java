30eed4f6844f0af666a16a7726674d7870eee36b
Felix Pechtl <felix.pechtl@ametras.com>
2024-11-19 12:58:12 +0100

    Made navigation-bar dynamic

--- src/hub/src/main/java/io/github/fi0x/hub/rest/ConnectionController.java
+++ src/hub/src/main/java/io/github/fi0x/hub/rest/ConnectionController.java
@@ -5,14 +5,13 @@ import io.github.fi0x.util.dto.ServiceDataDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @SessionAttributes({"username"})
 public class ConnectionController
@@ -25,5 +24,13 @@ public class ConnectionController
 		log.debug("registerService() called");
 
 		connectionService.registerService(request.getRemoteAddr(), requestDto);
+	}
+
+	@GetMapping("/api/service/list")
+	public List<ServiceDataDto> getRegisteredServices()
+	{
+		log.debug("getRegisteredServices() called");
+
+		return connectionService.getConnectedServices();
 	}
 }
