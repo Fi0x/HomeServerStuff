7b16cdf45f39375f45651d48fbc9dd9390b37658
Fi0x <felpech@aol.com>
2024-12-01 17:15:58 +0100

    Added new dto for general sensor-data

--- modules/data/src/main/java/io/github/fi0x/data/rest/CollectionController.java
+++ modules/data/src/main/java/io/github/fi0x/data/rest/CollectionController.java
@@ -1,6 +1,7 @@
 package io.github.fi0x.data.rest;
 
 import io.github.fi0x.data.logic.dto.DataDto;
+import io.github.fi0x.data.logic.dto.SensorDto;
 import io.github.fi0x.data.service.CollectionService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -12,17 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/upload")
+@RequestMapping("/api")
 @RequiredArgsConstructor
-public class DataCollectionController
+public class CollectionController
 {
 	private final CollectionService collectionService;
 
-	@PostMapping("/data")
+	@PostMapping("/upload")
 	public void getData(HttpServletRequest request, @RequestBody DataDto requestDto)
 	{
 		log.debug("getData() called");
 
 		collectionService.addData(request.getRemoteAddr(), requestDto);
+	}
+
+	@PostMapping("/register")
+	public void registerSensor(HttpServletRequest request, @RequestBody SensorDto requestDto)
+	{
+		//TODO: Register sensor for later faster data-transfer and save sensor data in new table to save space
 	}
 }
