6e1756c7bb5be9681213c9af7511012e53f9c545
Fi0x <felpech@aol.com>
2025-07-07 20:43:04 +0200

    Fixed icon

--- modules/sailing/src/main/java/io/github/fi0x/sailing/service/RaceService.java
+++ modules/sailing/src/main/java/io/github/fi0x/sailing/service/RaceService.java
@@ -24,7 +24,6 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-//TODO: Switch image-file to the correct icon
 @Slf4j
 @Service
 @RequiredArgsConstructor
