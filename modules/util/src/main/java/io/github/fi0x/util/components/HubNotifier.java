e0a6c3e1ccb3f18e479c85cb1b2c0999dcab3989
Felix Pechtl <felix.pechtl@ametras.com>
2025-10-06 10:41:45 +0200

    Updated hub-notifier to use broadcast address

--- modules/util/src/main/java/io/github/fi0x/util/components/HubNotifier.java
+++ modules/util/src/main/java/io/github/fi0x/util/components/HubNotifier.java
@@ -12,6 +12,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * This component is used to notify the hub about the existence of a new service.
  */
@@ -20,6 +28,9 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class HubNotifier
 {
+	private static final String URL_PREFIX = "http://";
+	private static final String HUB_REGISTER_ENDPOINT = "/api/service/register";
+
 	@Value("${server.port}")
 	private Integer currentPort;
 
@@ -39,22 +50,61 @@ public class HubNotifier
 	{
 		log.debug("Sending service-info to hub");
 
-		String url =
-				"http://" + serviceInformation.getHubIp() + ":" + serviceInformation.getHubPort() + "/api/service/register";
-		ServiceDataDto requestDto =
-				ServiceDataDto.builder().name(serviceInformation.getServiceName()).port(currentPort).build();
+		//		String urlEnding = ":" + serviceInformation.getHubPort() + HUB_REGISTER_ENDPOINT;
+		String urlEnding = HUB_REGISTER_ENDPOINT;
 
+		ServiceDataDto requestDto = ServiceDataDto.builder().name(serviceInformation.getServiceName()).port(currentPort)
+				.build();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-
 		HttpEntity<ServiceDataDto> request = new HttpEntity<>(requestDto, headers);
 
+		for (String ip : getBroadcastIps())
+		{
+			try
+			{
+				log.trace("Sending service-info to " + ip);
+				String url = URL_PREFIX + ip + urlEnding;
+				restTemplate.postForObject(url, request, Void.class);
+			} catch (RestClientException e)
+			{
+				log.warn("Could not reach hub because of exception: {}", e.getLocalizedMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static List<String> getBroadcastIps()
+	{
+		HashSet<String> broadcastAddresses = new HashSet<>();
 		try
 		{
-			restTemplate.postForObject(url, request, Void.class);
-		} catch(RestClientException e)
+			Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces();
+			while (list.hasMoreElements())
+			{
+				NetworkInterface iface = list.nextElement();
+				if (iface == null)
+					continue;
+				if (!iface.isLoopback() && iface.isUp())
+				{
+					for (InterfaceAddress address : iface.getInterfaceAddresses())
+					{
+						if (address == null)
+							continue;
+
+						InetAddress broadcast = address.getBroadcast();
+						if (broadcast != null)
+						{
+							broadcastAddresses.add(broadcast.getHostAddress());
+						}
+					}
+				}
+			}
+		} catch (SocketException e)
 		{
-			log.warn("Could not reach hub because of exception: {}", e.getLocalizedMessage());
+			throw new RuntimeException(e);
 		}
+
+		return broadcastAddresses.stream().toList();
 	}
 }
